cdc8254c919d0caeb9290ff148501da975e28d71
TEXflip <michele.tessari.wk@gmail.com>
2023-12-31 14:23:17 +0100

    a bit cleaner code for the maze

--- labirinto/main.js
+++ labirinto/main.js
@@ -1,12 +1,11 @@
-let dim = 4, queueMode = true, queueModeCreator = false, invSpeed = 1, speedV2 = 100;//fattore moltiplicativo
-let xMat = 0, yMat = 0, piv, oldPiv, exit;
+let dim = 64, queueMode = true, queueModeCreator = false, invSpeed = 1, speedV2 = 1;//fattore moltiplicativo
+let xMat = 0, yMat = 0, piv, oldPiv, exit, interval;
 let mat = [], stack = [], exitMat = [];
 
 function setup() {
-	frameRate(1024);
-	rectMode(CENTER);
-	//angleMode(DEGREES);
-	createCanvas(window.innerWidth, window.innerHeight);
+	frameRate(60);
+	createCanvas(window.innerWidth - 1, window.innerHeight - 1);
+	translate(dim * 0.25, dim * 0.25);
 	piv = new Vect(0, 0);
 	xMat = parseInt(width / dim) + 1;
 	yMat = parseInt(height / dim) + 1;
@@ -26,13 +25,14 @@ function setup() {
 	stack.push(piv.copy());
 	exit = new exiter(piv.x, piv.y);
 	noStroke();
-	fill(0, 0, 255, 180)
-	rect(piv.x * dim + dim / 2, piv.y * dim + dim / 2, dim, dim)
+	fill(0, 0, 255)
+	// rect(piv.x * dim, piv.y * dim, dim, dim)
 	textAlign(CENTER, CENTER);
 	textSize(dim / 3);
-	text("Start!", piv.x * dim + dim / 2, piv.y * dim + dim / 2);
+	text("Start!", piv.x * dim + dim * 0.5, piv.y * dim + dim * 0.5);
 	stroke(0);
-	setInterval(drawLab, invSpeed);
+	strokeWeight(2);
+	interval = setInterval(drawLab, invSpeed);
 }
 
 function drawLab() {
@@ -41,11 +41,16 @@ function drawLab() {
 			nextStep();
 		else if (exit.stack.length > 0)
 			exit.next();
+		// else if (exit.shortest_path.length > 0) {
+		// 	let temp = exit.shortest_path.shift();
+		// 	fill(255, 0, 0);
+		// 	circle(temp.x * dim + dim * 0.5, temp.y * dim + dim * 0.5, dim * 0.25);
+		// }
+		else {
+			// stop the loop
+			clearInterval(interval);
+		}
 	}
-}
-
-function mousePressed() {
-
 }
 
 function nextStep() {
@@ -57,8 +62,6 @@ function nextStep() {
 
 	let next = random(choice);
 	oldPiv = piv.copy();
-
-	//console.log(choice,next,xPiv,yPiv);
 
 	switch (next) {
 		case 0: piv.x--; break;//	1
@@ -77,7 +80,6 @@ function nextStep() {
 			piv = stack.shift();
 		else
 			piv = stack.pop();
-		//stroke(random(255),random(255),random(255));
 	}
 }
 
@@ -96,30 +98,38 @@ class exiter {
 		this.piv = new Vect(x, y)
 		this.stack = [];
 		this.stack.push(this.piv.copy());
-		this.scale = 1;
+		this.shortest_path = [];
 		this.haiPoppato = false;
 		this.color = 0;
+		this.left_free = () => mat[this.piv.x][this.piv.y] != 3 && mat[this.piv.x][this.piv.y + 1] != 1;
+		this.right_free = () => mat[this.piv.x + 1][this.piv.y] != 3 && mat[this.piv.x + 1][this.piv.y + 1] != 1;
+		this.up_free = () => mat[this.piv.x][this.piv.y] != 2 && mat[this.piv.x + 1][this.piv.y] != 0;
+		this.down_free = () => mat[this.piv.x][this.piv.y + 1] != 2 && mat[this.piv.x + 1][this.piv.y + 1] != 0;
 	}
 	next() {
 		let choice = [];
-		if (this.piv.x - 1 >= 0 && !exitMat[this.piv.x - 1][this.piv.y] && mat[this.piv.x][this.piv.y] != 3 && mat[this.piv.x][this.piv.y + 1] != 1) choice.push(0);
-		if (this.piv.y - 1 >= 0 && !exitMat[this.piv.x][this.piv.y - 1] && mat[this.piv.x][this.piv.y] != 2 && mat[this.piv.x + 1][this.piv.y] != 0) choice.push(1);
-		if (this.piv.x + 1 < xMat - 1 && !exitMat[this.piv.x + 1][this.piv.y] && mat[this.piv.x + 1][this.piv.y] != 3 && mat[this.piv.x + 1][this.piv.y + 1] != 1) choice.push(2);
-		if (this.piv.y + 1 < yMat - 1 && !exitMat[this.piv.x][this.piv.y + 1] && mat[this.piv.x][this.piv.y + 1] != 2 && mat[this.piv.x + 1][this.piv.y + 1] != 0) choice.push(3);
+		if (this.piv.x - 1 >= 0 && !exitMat[this.piv.x - 1][this.piv.y] && this.left_free()) choice.push(0);
+		if (this.piv.y - 1 >= 0 && !exitMat[this.piv.x][this.piv.y - 1] && this.up_free()) choice.push(1);
+		if (this.piv.x + 1 < xMat - 1 && !exitMat[this.piv.x + 1][this.piv.y] && this.right_free()) choice.push(2);
+		if (this.piv.y + 1 < yMat - 1 && !exitMat[this.piv.x][this.piv.y + 1] && this.down_free()) choice.push(3);
 
-		if ((this.piv.x - 1 == -1 && mat[this.piv.x][this.piv.y] != 3 && mat[this.piv.x][this.piv.y + 1] != 1)
-			|| (this.piv.y - 1 == -1 && mat[this.piv.x][this.piv.y] != 2 && mat[this.piv.x + 1][this.piv.y] != 0)
-			|| (this.piv.x + 1 == xMat - 1 && mat[this.piv.x + 1][this.piv.y] != 3 && mat[this.piv.x + 1][this.piv.y + 1] != 1)
-			|| (this.piv.y + 1 == yMat - 1 && mat[this.piv.x][this.piv.y + 1] != 2 && mat[this.piv.x + 1][this.piv.y + 1] != 0)) {
+		// if you find the exit
+		if ((this.piv.x == 0 && this.left_free())
+			|| (this.piv.y == 0 && this.up_free())
+			|| (this.piv.x + 1 == xMat - 1 && this.right_free())
+			|| (this.piv.y + 1 == yMat - 1 && this.down_free())) {
 			fill(0, 255, 0);
-			rect(this.piv.x * dim + dim / 2, this.piv.y * dim + dim / 2, dim * this.scale, dim * this.scale);
+			rect(this.piv.x * dim + 1, this.piv.y * dim + 1, dim - 2, dim - 2);
+			fill(0);
+			text("Exit!", this.piv.x * dim + dim * 0.5, this.piv.y * dim + dim * 0.5);
+			// for (let i = 0; i < this.stack.length; i++)
+			// 	this.shortest_path.push(this.stack[i].copy());
 		}
 
-		if (choice.length > 0)
-			if (this.haiPoppato) {
-				this.stack.push(this.piv.copy());
-				this.haiPoppato = false;
-			}
+		if (choice.length > 0 && this.haiPoppato) {
+			this.stack.push(this.piv.copy());
+			this.haiPoppato = false;
+		}
 
 		let next = random(choice);
 
@@ -131,12 +141,12 @@ class exiter {
 		}
 		if (choice.length > 0) {
 			this.stack.push(this.piv.copy());
-			colorMode(HSB,360,100,100,100);
+			colorMode(HSB, 360, 100, 100, 100);
 			let col = atan2(this.piv.y * dim - height / 2, this.piv.x * dim - width / 2);
 			fill(map(col, PI, -PI, 0, 360), 100, 100, 50);
 			colorMode(RGB);
 			noStroke();
-			rect(this.piv.x * dim + dim / 2, this.piv.y * dim + dim / 2, dim * this.scale, dim * this.scale);
+			rect(this.piv.x * dim + 1, this.piv.y * dim + 1, dim, dim);
 			exitMat[this.piv.x][this.piv.y] = true;
 		}
 		else {
